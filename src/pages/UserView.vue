79198878d5ea2ba4f4a79fc0caf617b96d1065ee
Davide <d@vide.bz>
2019-12-03 23:50:33 +0100

    list of groups in user page added

--- src/pages/UserView.vue
+++ src/pages/UserView.vue
@@ -57,6 +57,35 @@
             </form>
           </div>
         </div>
+        <div class="row beacon-display m-4 p-4 position-relative" v-show="user.groups.length > 0">
+          <div class="col-12 d-flex flex-column flex-grow-1 flex-shrink-0">
+            <div class="row">
+              <div class="col pl-0">
+                <h5>Groups</h5>
+              </div>
+            </div>
+            <div class="row">
+              <div class="table-responsive mt-3 table-groups-role-wrapper d-flex flex-column">
+                <table class="table table-groups-role">
+                  <thead>
+                  <tr class="col-header table-header pl-0 pr-0">
+                    <th scope="col">Id</th>
+                    <th scope="col">Name</th>
+                    <th scope="col">Role</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                  <tr class="user-item" v-bind:key="groupRole.group.id" v-if="user.groups.length" v-for="groupRole in user.groups" @click.prevent.stop="showGroup(groupRole.group)">
+                    <td class="align-middle" scope="row">{{ groupRole.group.id }}</td>
+                    <td class="align-middle">{{ groupRole.group.name }}</td>
+                    <td class="align-middle">{{ groupRole.role }}</td>
+                  </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
       <loader :visible="!loaded" :label="'Loading user data...'"/>
       <loader :visible="saving" :label="'Saving users...'"/>
@@ -84,7 +113,8 @@ export default {
         id: '',
         name: '',
         surname: '',
-        email: ''
+        email: '',
+        groups: []
       },
       loaded: false,
       saving: false,
@@ -122,6 +152,9 @@ export default {
     },
     isSelf() {
       return this.getUsername === this.user.username
+    },
+    showGroup(group) {
+      router.push({ name: 'group-edit', params: { id: group.id }})
     }
   }
 }
@@ -129,6 +162,8 @@ export default {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
+  @import '../variables';
+
   div.form-group {
     margin-bottom: 1rem;
   }
@@ -139,5 +174,50 @@ export default {
 
   .btn {
     font-size: 1em;
+  }
+
+  .table-header {
+    background-color: $background-grey;
+    color: $grey;
+    padding-top: 1em;
+    padding-bottom: 1em;
+    font-size: 0.8rem;
+  }
+
+  .table-groups-role-wrapper {
+    position: relative;
+    min-height: 180px;
+
+    .table-groups-role {
+
+      position: absolute;
+      top: 0;
+      bottom: 0;
+      left: 0;
+      right: 0;
+      padding-bottom: 4em;
+
+      thead {
+        border-bottom: none;
+
+        .table-header {
+          border-bottom: none;
+
+          th {
+            border-bottom: none;
+          }
+        }
+      }
+
+      .user-item {
+
+        td {
+          border-bottom: 1px solid $background-grey;
+          font-size: 0.8rem;
+          color: $text-grey;
+        }
+
+      }
+    }
   }
 </style>
--- src/pages/UsersView.vue
+++ src/pages/UsersView.vue
@@ -183,6 +183,7 @@ export default {
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style lang="scss" scoped>
   @import '../variables';
+  @import '../variables';
   h1 {
     display: inline-block;
   }
