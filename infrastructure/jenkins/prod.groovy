78edafde57b202292a9251c85bf0cf7edc773adf
Peter Moser <p.moser@noi.bz.it>
2021-01-14 18:18:27 +0100

    infra: use same node version in local development and remote

--- infrastructure/jenkins/prod.groovy
+++ infrastructure/jenkins/prod.groovy
@@ -1,18 +1,19 @@
 pipeline {
     agent {
         dockerfile {
-            filename 'docker/dockerfile-node'
-            additionalBuildArgs '--build-arg JENKINS_USER_ID=`id -u jenkins` --build-arg JENKINS_GROUP_ID=`id -g jenkins`'
+            filename 'infrastructure/docker/node.dockerfile'
+            additionalBuildArgs '--build-arg JENKINS_USER_ID=$(id -u jenkins) --build-arg JENKINS_GROUP_ID=$(id -g jenkins)'
         }
     }
-
+	options {
+        ansiColor('xterm')
+    }
     environment {
         AWS_ACCESS_KEY_ID = credentials('AWS_ACCESS_KEY_ID')
         AWS_SECRET_ACCESS_KEY = credentials('AWS_SECRET_ACCESS_KEY')
         VUE_APP_API_BASE_URL = "https://api.beacon.bz.it"
         VUE_APP_SUPPORT_EMAIL = "support@beacon.bz.it"
     }
-
     stages {
         stage('Dependencies') {
             steps {
